343077fd76bf53a945665b71bcc59b738a9bc9e0
박세원 <parkse9414@naver.com>
2014-12-10 21:01:14 +0900

    s

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 var img; 
+var count;
 
 function setup()
 {
     createCanvas(1040,1450);
     img = loadImage("bg.png");  
     img2 = loadImage("man.png");
+    img3 = loadImage("star.png");
+    count=0;
 }
 
 
@@ -22,7 +25,15 @@ function draw()
     //남자
     image(img2,365 + Math.cos(timing*2*PI)*15+ Math.cos(timing*2*PI)*5,           // x좌표
     744 + Math.sin(timing*2*PI)*15+ Math.cos(timing*2*PI)*5);                     // y좌표
-  
+
+    drawstar(count,725);
+    coun=coun+1;
+    if(count>1040){
+        count=0;
+    }
 }
 
 
+function drawstar(x,y){
+    image(img3,x-img3.width/2,y-img3.height/2);
+}
