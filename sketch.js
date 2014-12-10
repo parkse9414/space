f5892276cb2368e530a283e24bd7989be2f24224
박세원 <parkse9414@naver.com>
2014-12-10 20:25:16 +0900

    d

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ function setup()
 {
     createCanvas(1040,1554);
     img = loadImage("bg.png");  
+    img2 = loadImage("man.png");
 }
 
 function draw()
@@ -11,5 +12,13 @@ function draw()
     background(img);
     noFill();
     noStroke();
+
+    var duration = 800;
+    var timing = (new Date()%duration)/duration;
+
+    image(img2,710 + Math.cos(timing*2*PI)*25+ Math.cos(timing*2*PI)*5,           // x좌표
+              321 + Math.sin(timing*2*PI)*25+ Math.cos(timing*2*PI)*5,            // y좌표
+              15,                                                                                                                                           // width
+              15);     
 }
 
