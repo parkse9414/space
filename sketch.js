dbdb9fd590ebe0b980e8cd22fd5b67322c829f7c
박세원 <parkse9414@naver.com>
2014-12-11 01:07:52 +0900

    s

--- sketch.js
+++ sketch.js
@@ -36,12 +36,13 @@ function draw()
         image(img5,0,0);
     }else if(f===1){
         image(img4,0,0);
-    }else if(f===1){
-    	image(img6,0,0);
-    } 
+    }
 
+    var duration = 8500;
+    var timing = (new Date()%duration)/duration;
+    image(img6, 324 + Math.cos(timing*2*PI)*10, 534 + Math.sin(timing*2*PI)*10); 
 
-
+    
     var duration = 8000;
     var timing = (new Date()%duration)/duration;
 
