7610cca5369a16f6ff96100dfe12eb39750020b5
박세원 <parkse9414@naver.com>
2014-12-11 01:41:00 +0900

    g

--- sketch.js
+++ sketch.js
@@ -77,8 +77,8 @@ function object(){
     image(img7, 13 + Math.cos(timing*2*PI)*3, 739 + Math.sin(timing*2*PI)*5); 
 
     image(img9,107,count2+631);
-    count2=count2+1;
-    if(count2=>600){
+    count2=count2-1;
+    if(count2>600){
        count2=0;
     }  
 }
