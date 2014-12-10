e618741372886677e2169d377c8c840ed8ae5a2d
박세원 <parkse9414@naver.com>
2014-12-10 20:26:28 +0900

    s

--- sketch.js
+++ sketch.js
@@ -13,12 +13,11 @@ function draw()
     noFill();
     noStroke();
 
-    var duration = 800;
+    var duration = 8000;
     var timing = (new Date()%duration)/duration;
 
     image(img2,710 + Math.cos(timing*2*PI)*25+ Math.cos(timing*2*PI)*5,           // x좌표
-              321 + Math.sin(timing*2*PI)*25+ Math.cos(timing*2*PI)*5,            // y좌표
-              15,                                                                                                                                           // width
-              15);     
+              321 + Math.sin(timing*2*PI)*25+ Math.cos(timing*2*PI)*5);           // y좌표
+  
 }
 
