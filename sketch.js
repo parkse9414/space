a4749e4eea88e15637a48ec3c0e2e2020ffc6166
박세원 <parkse9414@naver.com>
2014-12-11 10:43:02 +0900

    s

--- sketch.js
+++ sketch.js
@@ -53,13 +53,6 @@ function draw()
         object();
     }
 
-    mouse();
-    
-    var duration = 8000;
-    var timing = (new Date()%duration)/duration;
-
-    image(img2,365 + Math.cos(timing*2*PI)*15+ Math.cos(timing*2*PI)*5,         
-    744 + Math.sin(timing*2*PI)*15+ Math.cos(timing*2*PI)*5);       
 
     
 }
@@ -112,11 +105,4 @@ function object(){
     if(count4>300){
        count4=0;
     }
-}
-
-function mouse(){
-    var duration = 4500;
-    var timing = (new Date()%duration)/duration;
-    image(img16,500 + Math.cos(timing*2*PI)*15+ Math.cos(timing*2*PI)*5,         
-    500 + Math.sin(timing*2*PI)*15+ Math.cos(timing*2*PI)*5);
 }
