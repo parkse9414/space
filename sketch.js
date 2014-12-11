40ecb00503a910906eb6d65a5557d84ada8b9e0d
박세원 <parkse9414@naver.com>
2014-12-11 09:54:48 +0900

    d

--- sketch.js
+++ sketch.js
@@ -85,8 +85,8 @@ function object(){
     var duration = 8000;
     var timing = (new Date()%duration)/duration;
     image(img10, 756 + Math.cos(timing*2*PI)*15, 864 + Math.sin(timing*2*PI)*7); 
-    image(img11, 823+ Math.cos(timing*2*PI)*10, 921 + Math.sin(timing*2*PI)*2); 
-    image(img12, 800+ Math.cos(timing*2*PI)*12, 998 + Math.sin(timing*2*PI)*3); 
+    image(img11, 823+ Math.cos(timing*2*PI)*20, 941 + Math.sin(timing*2*PI)*2); 
+    image(img12, 780+ Math.cos(timing*2*PI)*15, 998 + Math.sin(timing*2*PI)*3); 
     image(img13, 276+ Math.cos(timing*2*PI)*10, 508 + Math.sin(timing*2*PI)*2); 
     image(img14, 230+ Math.cos(timing*2*PI)*12, 801 + Math.sin(timing*2*PI)*3); 
 
